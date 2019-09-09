a56cde0baddd17170117a5ff7dbc48ab8cbbe230
Viachaslau Pashaliuk <v.pashaliuk@gmail.com>
2019-09-09 22:04:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,3 +21,23 @@ end
 def shift_with_args(arr)
   str.shift(2)
 end
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr, element)
+  arr[3].insert(element)
+end
+
+def using_unique(arr)
+  str.unique
+end
+
+def using_flatten(arr)
+  str.flatten
+end
+
+def using_delete(arr, str)
+  str.delete(str)
+end
